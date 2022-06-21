14b2de5865829165fc361d2eb1d7d01f8afac5dd
Sagar Chatur <sagarchatur4444@gmail.com>
2022-06-21 22:34:35 +0530

    no

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [user,setUser] = useState([]);
+
+  const fetchData =()=>{
+    fetch('https://randomuser.me/api/?results=1')
+    .then((response) =>{
+        return response.json();
+    }).then((data)=>{
+        let gagan = data.results
+        console.log(gagan);
+        setUser(gagan)
+    })
+}
+useEffect(()=>{
+    fetchData();
+},[])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div>
+       {user.map(data => (
+        <div className="col-md-4 animated fadeIn" key={data.id.value}>
+              <div className="card">
+                <div className="card-body">
+                  <div className="avatar">
+                    <img
+                      src={data.picture.large}
+                      className="card-img-top"
+                      alt=""
+                    />
+                  </div>
+                  <h5 className="card-title">
+                    {data.name.first +
+                      " " +
+                      data.name.last}
+                  </h5>
+                  <p className="card-text">
+                    {data.location.city +
+                      ", " +
+                      data.location.state}
+                    <br />
+                    <span className="phone">{data.phone}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+        ))}
+       </div>
       </header>
     </div>
   );
